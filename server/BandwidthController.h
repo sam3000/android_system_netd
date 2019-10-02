8e00e82e2538c0155c1026c53dc0f92f54990bba
Sam Mortimer <sam@mortimer.me.uk>
2019-10-02 15:52:18 -0700

    Revert "netd: Consolidate restrict apps methods"
    This reverts commit 09e8f9dc441c3c0f67321295d1a5a7f0ee9499ed.

--- server/BandwidthController.h
+++ server/BandwidthController.h
@@ -52,10 +52,12 @@ public:
     int addNiceApps(int numUids, char *appUids[]);
     int removeNiceApps(int numUids, char *appUids[]);
 
-    int addRestrictAppsOnInterface(const std::string& usecase, const std::string& iface,
-                                   int numUids, char *appUids[]);
-    int removeRestrictAppsOnInterface(const std::string& usecase, const std::string& iface,
-                                      int numUids, char *appUids[]);
+    int addRestrictAppsOnData(const std::string& iface, int numUids, char *appUids[]);
+    int removeRestrictAppsOnData(const std::string& iface, int numUids, char *appUids[]);
+    int addRestrictAppsOnVpn(const std::string& iface, int numUids, char *appUids[]);
+    int removeRestrictAppsOnVpn(const std::string& iface, int numUids, char *appUids[]);
+    int addRestrictAppsOnWlan(const std::string& iface, int numUids, char *appUids[]);
+    int removeRestrictAppsOnWlan(const std::string& iface, int numUids, char *appUids[]);
 
     int setGlobalAlert(int64_t bytes);
     int removeGlobalAlert();
@@ -94,8 +96,9 @@ public:
 
     std::string makeDataSaverCommand(IptablesTarget target, bool enable);
 
-    int manipulateRestrictAppsInOut(const std::string& usecase, const std::string& iface,
-                                    const std::vector<std::string>& appStrUids, IptOp appOp);
+    int manipulateRestrictAppsInOut(const std::string& iface,
+                                    const std::vector<std::string>& appStrUids,
+                                    std::vector<int /*appUid*/>& restrictAppUids, IptOp appOp);
 
     int manipulateRestrictApps(const std::vector<std::string>& appStrUids, const std::string& chain,
                                std::vector<int /*appUid*/>& restrictAppUids, IptOp appOp);
@@ -152,7 +155,9 @@ public:
     std::map<std::string, QuotaInfo> mQuotaIfaces;
     std::set<std::string> mSharedQuotaIfaces;
 
-    std::map<std::string /* interface name*/, std::vector<int /*appUid*/>> mRestrictAppsOnInterface;
+    std::vector<int /*appUid*/> restrictAppUidsOnData;
+    std::vector<int /*appUid*/> restrictAppUidsOnVpn;
+    std::vector<int /*appUid*/> restrictAppUidsOnWlan;
 };
 
 #endif
